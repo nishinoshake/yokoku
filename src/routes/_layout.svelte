4d4a10668f29ca52cd7fe4ac3febbf0dc2f6a6d7
Tatsuya Kinno <lawson.and.7.11@gmail.com>
2020-01-19 22:47:14 +0900

    what-inputをグローバルでいれてしまう
    IE11でDynamic importのポリフィルでエラーが出て面倒だったのでいっそ

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,11 +1,3 @@
-<script>
-  import { onMount } from 'svelte'
-
-  onMount(async () => {
-    await import('what-input')
-  })
-</script>
-
 <style lang="scss" global>
   @import 'styles/global.scss';
 </style>
